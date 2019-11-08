80c4214e4467355ec1c7034dd427956419656eeb
lenoxcyy13 <lenoxcyy13@gmail.com>
2019-11-09 00:29:11 +0800

    finally!

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true}));
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', function(req, res) {
     res.sendfile('./login.html');
 });
 
 app.post('/send', urlencodedParser, function(req, res){
-    res.send('your name: ', req.body.name);
+    res.send('your name: ' + req.body.name);
 })
 
-app.listen(port, () => console.log('listening on port ${port}'));
+app.listen(3000, () => console.log('listening on port 3000'));
