fca63c745e84a4d2e043a9a0356d8a527795a8f5
Jason Lien <lienshao@tamu.edu>
2019-11-11 14:38:39 -0600

    configured database

--- create_db.js
+++ create_db.js
@@ -1,13 +1,13 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("users.db");
+const db = new sqlite3.Database("tables.db");
 db.serialize(() => {
-  db.run("CREATE TABLE users (userName TEXT , userPassword TEXT)");
+  db.run("CREATE TABLE users (userName TEXT PRIMARY KEY, userPassword TEXT)");
   db.run("INSERT INTO users VALUES ('Jason', '12341')");
   db.run("INSERT INTO users VALUES ('Michael', 'password')");
   db.run("INSERT INTO users VALUES ('Lenox', 'giff')");
 
   db.run(
-    "CREATE TABLE events (eventUser TEXT, eventName TEXT, eventDescription TEXT, eventDate TEXT, eventTime TEXT, FOREIGN KEY(eventUser) REFERENCES users(userName))"
+    "CREATE TABLE events (eventUser TEXT , eventName TEXT, eventDescription TEXT, eventDate TEXT, eventTime TEXT, FOREIGN KEY(eventUser) REFERENCES users(userName))"
   );
   db.run(
     "INSERT INTO events VALUES ('Jason','CSCE 431', 'software engr class', '03/12/2019','08:00a')"
@@ -18,8 +18,11 @@ db.serialize(() => {
   db.run(
     "INSERT INTO events VALUES ('Lenox','CSCE 101', 'data structure and algorithm', '06/12/2019','10:00a')"
   );
+  db.each("SELECT userName FROM users", (err, row) => {
+    console.log(row.userName);
+  });
   db.each("SELECT eventUser, eventName FROM events", (err, row) => {
-    console.log(row.eventUser, row.eventName);
+    console.log(row.eventUser, "\t", row.eventName);
   });
 });
 db.close();
